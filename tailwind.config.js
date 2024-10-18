15d7638ce6819f180b6350def0242d09bef15113
Ferli Suganda <160320483+DeoVindice29@users.noreply.github.com>
2024-10-18 18:26:20 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,6 @@ tailwind.config = {
                 Outfit: ["Outfit", "sans-serif"],
                 Ovo: ["Ovo", "serif"],
             },
-            animation: {
-                spin_slow: "spin 6s linear infinite",
-            },
             colors: {
                 lightHover: "#f4fff4",
                 darkHover: "#2a004a",
